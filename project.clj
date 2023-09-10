b681640a73a1f365ccb2184d8f03bd82792aaa41
James Reeves <jreeves@weavejester.com>
2023-09-10 19:44:34 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject org.ring-clojure/ring-websocket-async "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "1.6.681"]
+                 [ring/ring-core "1.11.0-alpha1"]])
---
+++ src/ring/websocket/async.clj
@@ -0,0 +1,6 @@
+(ns ring.websocket.async)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
