afd9d16808790791a0ce6d5f369df5c51e59d466
James Reeves <jreeves@weavejester.com>
2023-09-23 19:56:42 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.ring-clojure/ring-websocket-async "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for using core.async with Ring's websocket API"
+  :url "https://github.com/ring-clojure/ring-websocket-async"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
